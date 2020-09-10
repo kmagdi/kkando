120bc400cedbec20f1ff88539f2fd11bb5f96be5
kmagdi <katay.magdi@gmail.com>
2020-09-11 00:12:29 +0200

    fuggosegek

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter,Route,Switch} from "react-router-dom"
+import {Szak1} from "./components/Szak1"
+import {Szak2} from "./components/Szak2"
+import {Szak3} from "./components/Szak3"
+import {Home} from "./components/Home"
+import {Navbar} from "./components/Navbar"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export const App=()=>{
+    return(
+        <BrowserRouter>
+            <Navbar/>
+                <Switch>
+                    <Route  path="/" component={Home}/> 
+                    <Route  path="/szak1" component={Szak1}/> 
+                    <Route path="/szak2" component={Szak2}/>
+                    <Route path="/szak3" component={Szak3}/>     
+                </Switch>
+        </BrowserRouter>
+        
+    )
 }
-
-export default App;
