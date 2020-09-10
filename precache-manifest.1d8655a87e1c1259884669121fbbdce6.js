0d2165a3d28e89cbf9eb80467e2da73dfec36c2b
kmagdi <katay.magdi@gmail.com>
2020-09-10 23:42:46 +0200

    Updates

---
+++ precache-manifest.1d8655a87e1c1259884669121fbbdce6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e4822f31e43401164c538e5683212c",
+    "url": "/kkando/index.html"
+  },
+  {
+    "revision": "4d759a8dd38a15dbff0a",
+    "url": "/kkando/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3ec6a00de12724bcf942",
+    "url": "/kkando/static/js/2.aec09ab6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kkando/static/js/2.aec09ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d759a8dd38a15dbff0a",
+    "url": "/kkando/static/js/main.9f889697.chunk.js"
+  },
+  {
+    "revision": "e7e8333ae4ae7d8462f5",
+    "url": "/kkando/static/js/runtime-main.d0f3f99f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kkando/static/media/logo.5d5d9eef.svg"
+  }
+]);
