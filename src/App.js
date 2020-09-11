2b7d0ff2c86a33f88e03ac0e1421eea8d83ef858
sataerik <sataerik@gmail.com>
2020-09-11 21:56:21 +0200

    added parallax for testing

--- src/App.js
+++ src/App.js
@@ -6,19 +6,56 @@ import {Szak3} from "./components/Szak3"
 import {Home} from "./components/Home"
 import {Navbar} from "./components/Navbar"
 
-
+import {ParallaxProvider,ParallaxBanner} from 'react-scroll-parallax'
+import img1 from './eszt.png'
+import img2 from './eszt2.jpg'
+import logo from './logo.png'
 
 export const App=()=>{
     return(
-        <BrowserRouter>
-            <Navbar/>
+        <div> {/* itt egyelőre csak felrakom az eddigi parallaxot */}
+            <BrowserRouter>
+                <Navbar/>
                 <Switch>
-                    <Route  path="/" component={Home}/> 
-                    <Route  path="/szak1" component={Szak1}/> 
+                    <Route path="/" component={Home}/> 
+                    <Route path="/szak1" component={Szak1}/> 
                     <Route path="/szak2" component={Szak2}/>
                     <Route path="/szak3" component={Szak3}/>     
                 </Switch>
-        </BrowserRouter>
-        
+            </BrowserRouter>
+            
+            <ParallaxProvider>
+                <div id="top-banner-container">
+                    <ParallaxBanner
+                        className="parallax-panel"
+                        layers={[
+                            {
+                                image: img1,
+                                amount:0.15
+                            }
+                        ]}
+                        style={{
+                            height:'100vh'
+                        }}
+                    >
+                        <img src={logo} id="welcome-image-logo" className="fade-in"></img>
+                    </ParallaxBanner>
+                </div>
+                <div style={{backgroundColor:'#eee',width:'100%',height:'300px'}}></div>
+                <ParallaxBanner
+                    className="parallax-panel"
+                    layers={[
+                        {
+                            image: img2,
+                            amount:0.25
+                        }
+                    ]}
+                    style={{
+                        height:'100vh'
+                    }}
+                ></ParallaxBanner>
+                {/*<div style={{backgroundColor:'rgba(255,0,0,0.2)',height:'1000px'}}></div>*/}
+            </ParallaxProvider>
+        </div>
     )
 }
