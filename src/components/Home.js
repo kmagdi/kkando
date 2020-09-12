560ae82b173947edec4ddb9752dfdd5d448291ab
kmagdi <katay.magdi@gmail.com>
2020-09-13 00:02:16 +0200

    routing - home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CollectData from "./CollectData"
 import { MyCarousel } from './MyCarousel';  
+import { MyParallax } from './MyParallax';
 
 export const Home=()=>{
     return(
         <div>
+            {/*<MyParallax/>*/}
             <div className="row justify-content-center"><div className="col-md-6 m-2"><MyCarousel/></div></div>
             <CollectData/>
         </div>
