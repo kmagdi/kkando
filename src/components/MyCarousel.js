d484e7b020f2d6d3a792e9596f3b5c801d655fbd
vargabrigitta <brigi92477@gmail.com>
2020-09-14 17:26:50 +0200

    carousel css

--- src/components/MyCarousel.js
+++ src/components/MyCarousel.js
@@ -8,39 +8,7 @@ import './MyCarousel.css'
 export const MyCarousel=()=>{
 
     return (
-            <Carousel autoPlay className="justify-content-center" >
-                {/*<div>
-                    <img src={require('./assets/1.jpg')} alt="kep1"/>
-                    <p className="legend "><span className="h1" ><Link to={"/szak1"}>Szoftverfejlesztő</Link></span></p>
-                </div>
-                <div>
-                    <img src={require('./assets/2.jpg')} alt="kep2"/>
-                    <p className="legend"><span className="h1" >Mechatronikai technikus</span></p>
-                </div>
-                <div>
-                    <img src={require('./assets/3.jpg')} alt="kep3" />
-                    <p className="legend"><span className="h1">Legend 3</span></p>
-                </div>
-                <div>
-                    <img src={require('./assets/4.jpg')} alt="kep4"/>
-                    <p className="legend"><span className="h1">Legend 4</span></p>
-                </div>
-                <div>
-                    <img src={require('./assets/5.jpg')} alt="kep5"/>
-                    <p className="legend"><span className="h1">Elektronikai technikus</span></p>
-                </div>
-                <div>
-                    <img src={require('./assets/6.jpg')} alt="kep6" />
-                    <p className="legend"><span className="h1">Gépgyártás-technológiai technikus</span></p>
-                </div>
-                <div>
-                    <img src={require('./assets/7.jpg')} alt="kep7"/>
-                    <p className="legend"><span className="h1">Hegesztő</span></p>
-                </div>
-                <div>
-                    <img src={require('./assets/8.jpg')} alt="kep8" />
-                    <p className="legend"><span className="h1">Képző- és iparművészeti munkatárs</span></p>
-                </div>*/}
+            <Carousel autoPlay className="justify-content-center">
                 <Carousel.Item>
                     <img src={require('./assets/T1.jpg')} alt="kep1" />
                     <Carousel.Caption>
