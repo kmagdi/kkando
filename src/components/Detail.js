fbf2f6e9674fe4ba0f21e7009657ef48668ce01d
sataerik <sataerik@gmail.com>
2020-09-15 10:15:34 +0200

    made the index->szak transition smoother

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -5,14 +5,15 @@ import { MyParallax } from './MyParallax';
 
 export const Detail=(props)=>{
     // scroll to top
+    document.getElementsByTagName('html')[0].setAttribute('smooth','false');
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-    return(     
+    {document.getElementsByTagName('html')[0].setAttribute('smooth','true')}
+    return(
         <React.Fragment>
             <FullscreenBanner title={props.adatok.leiras} />
             {Object.keys(props.temaKorok).filter(obj=>props.temaKorok[obj].length>5).map((obj, i) => <Box key={obj} tema={props.temaKorok[obj]}/>)}
         </React.Fragment>
-        
     )
 }
 
