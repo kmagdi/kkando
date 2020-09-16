db444f99dd37f30fa9e1a5f1096fa172bb020fc5
kmagdi <katay.magdi@gmail.com>
2020-09-16 23:06:36 +0200

    egy kis zene  :)

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -2,17 +2,20 @@ import React from 'react';
 import {Box} from "./Box"
 import { FullscreenBanner } from './FullscreenBanner';
 import './Details.css'
-
+import Music from "./Music";
 
 
 export const Detail=(props)=>{
+    console.log("./assets/"+props.adatok.kod+".mp3")
+
     return(
         <React.Fragment>
-             
+           
             <FullscreenBanner title={props.adatok.leiras} />
-            <div className="col">
+            <div className="col"> <Music />
                 {Object.keys(props.temaKorok).filter(obj=>props.temaKorok[obj].length>5).map((obj, i) => <Box key={obj} tema={props.temaKorok[obj]} id={obj} adatok={props.adatok}/>)}
-            </div>
+            </div>  
+           
             <div className="col">
                 {props.fotok.map((e,i) => <img key={e} src={e} alt="foto"/>)}
             </div>
---
+++ src/components/Music.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactHowler from 'react-howler'
+import zene from "./assets/T1.mp3"
+
+
+class Music extends React.Component {
+  constructor () {
+   super()
+    this.state = {
+      playing: false
+    }
+    this.handlePlay = this.handlePlay.bind(this)
+    this.handlePause = this.handlePause.bind(this)
+  }
+
+  handlePlay () {
+    this.setState({
+      playing: true
+    })
+  }
+
+  handlePause () {
+    this.setState({
+      playing: false
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <ReactHowler
+          src={[zene]}
+          playing={this.state.playing}
+        />
+        <button onClick={this.handlePlay}>Play</button>
+        <button onClick={this.handlePause}>Pause</button>
+      </div>
+    )
+  }
+}
+
+export default Music
