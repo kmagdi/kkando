62591dd7a2358c1be1d117ed162f28d6eedb17eb
vargabrigitta <brigi92477@gmail.com>
2020-09-16 22:22:04 +0200

    footer és detail alakítás

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -13,8 +13,8 @@ export const Detail=(props)=>{
             <div className="col">
                 {Object.keys(props.temaKorok).filter(obj=>props.temaKorok[obj].length>5).map((obj, i) => <Box key={obj} tema={props.temaKorok[obj]} id={obj} adatok={props.adatok}/>)}
             </div>
-            <div className="col">
-                {props.fotok.map((e,i) => <img key={e} src={e} alt="foto"/>)}
+            <div className="col" style={{padding:'0'}}>
+                {props.fotok.map((e,i) => <img key={e} src={e} alt="foto" style={{maxWidth:'100vw'}} />)}
             </div>
             
         </React.Fragment>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,11 @@ export const Footer=(props)=> {
         <div>
             <div id="footer" className="row">
                 <div className="col-lg-6 footer-col footer-col-first">
-                    <p><b>Székhely:</b> {props.contactData.location}</p>
-                    <p><b>Levelezési cím:</b> {props.contactData.adress}</p>
-                    <p><b>Telefon:</b> {props.contactData.tel}</p>
-                    <p><b>E-mail:</b><br/><span className="tab">&nbsp;&nbsp;&nbsp;&nbsp;</span>{props.contactData.email}</p>
-                    <p><b>OM azonosító:</b> {props.contactData.omid}</p>        
+                    <p><b>Székhely:</b><br/>{props.contactData.location}</p>
+                    <p><b>Levelezési cím:</b><br/>{props.contactData.adress}</p>
+                    <p><b>Telefon:</b><br/>{props.contactData.tel}</p>
+                    <p><b>E-mail:</b><br/>{props.contactData.email}</p>
+                    <p><b>OM azonosító:</b><br/>{props.contactData.omid}</p>        
                 </div>
                 <div className="col-lg-6 footer-col footer-col-second" style={{textAlign:'center',fontSize:'125%'}}>
                     <p>Valami "középre" igazított szöveg</p>
