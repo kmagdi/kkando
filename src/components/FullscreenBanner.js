2bbac46b9114dcabeb2adf2b4243ef036af8c095
sataerik <sataerik@gmail.com>
2020-10-12 18:29:13 +0200

    hopefully fixed FullscreenBanner logo scaling
    it's ugly but it works.. at least for now

--- src/components/FullscreenBanner.js
+++ src/components/FullscreenBanner.js
@@ -4,15 +4,49 @@ import {MyParallax} from './MyParallax';
 export const FullscreenBanner = (props)=>{
     const imageExists = props.image!==undefined;
     const titleExists = props.title!==undefined;
-    useEffect(()=>{
-        let img = new Image();
-        img.src = props.image;
-        img.onload = (i) => {
-            updateImage();
-            window.addEventListener('resize',()=>{updateImage();})
+
+    let img = new Image();
+    img.src = props.image;
+    img.onload = (i) => {
+        window.addEventListener('resize',()=>updateImage(img.src));
+        updateImage(img.src);
+    }
+
+    const updateImage = (x) => {
+        const obj = document.querySelector('#welcome-image-logo');
+        if(obj !== null){
+            obj.setAttribute('src',x);
+            const windowW = Math.round(window.innerWidth*0.9);
+            const windowH = Math.round(window.innerHeight*0.5);
+            if(obj.clientWidth > obj.clientHeight){
+                obj.setAttribute('style','height:10rem;');
+                console.log('width: ' + obj.clientWidth + ', height: ' + obj.clientHeight + '\nwWidth: ' + windowW + ', wHeight: ' + windowH);
+                if(obj.clientWidth > windowW){
+                    obj.setAttribute('class','fade-in-img logo-maxw');
+                }else if(obj.clientHeight > windowH){
+                    obj.setAttribute('class','fade-in-img logo-maxh');
+                }else{
+                    obj.setAttribute('class','fade-in-img logo-hor');
+                }
+                obj.setAttribute('style','');
+            }else if(obj.clientWidth < obj.clientHeight){
+                obj.setAttribute('style','width:10rem;');
+                //console.log('width: ' + obj.clientWidth + ', height: ' + obj.clientHeight + '\nwWidth: ' + windowW + ', wHeight: ' + windowH);
+                if(obj.clientWidth > windowW){
+                    obj.setAttribute('class','fade-in-img logo-maxw');
+                }else if(obj.clientHeight > windowH){
+                    obj.setAttribute('class','fade-in-img logo-maxh');
+                }else{
+                    obj.setAttribute('class','fade-in-img logo-ver');
+                }
+                obj.setAttribute('style','');
+            }else{
+                obj.setAttribute('class','fade-in-img logo-q');
+            }
         }
-    });
-    const updateImage = () => {
+        return obj;
+    };
+    /*const updateImage = () => {
         const obj = document.querySelector('#welcome-image-logo');
         if(obj !== null){
             const windowWidth = Math.round(window.innerWidth*0.9);
@@ -30,7 +64,8 @@ export const FullscreenBanner = (props)=>{
             }
         }
         return '';
-    };
+    };*/
+
     return (
         <MyParallax
             div="top-banner-container"
@@ -41,7 +76,7 @@ export const FullscreenBanner = (props)=>{
             <div className="kozep">
                 {imageExists?
                     <>
-                        <img src={props.image} id="welcome-image-logo" alt="logo" className="fade-in-img"></img>
+                        <img /*src={img.src}*/ id="welcome-image-logo" alt="logo" className="fade-in-img"></img>
                     </>
                 : null}
                 {titleExists?
@@ -51,7 +86,7 @@ export const FullscreenBanner = (props)=>{
                         <h1 id="welcome-title" className="fade-in-h">{props.title}</h1>
                 : null}
             </div>
-            {updateImage()}
+            {/*updateImage()*/}
         </MyParallax>
     );
 };
