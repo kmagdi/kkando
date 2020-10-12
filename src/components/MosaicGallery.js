e33cde4d6e1aed96a7fcc0292a8990c50c9ca909
sataerik <sataerik@gmail.com>
2020-10-12 18:46:19 +0200

    mosaic thumbnail mobile size + removed animation

--- src/components/MosaicGallery.js
+++ src/components/MosaicGallery.js
@@ -3,10 +3,10 @@ import './MosaicGallery.css';
 
 export const MosaicGallery = (props) => {
     return (
-        <div ref={props.animRef} id={props.id} className="mosaic-gallery">
+        <div /*ref={props.animRef}*/ id={props.id} className="mosaic-gallery">
             <h2 style={{marginBottom:'0.75rem'}}>{props.title}</h2>
             {[...props.images].map((i,index)=>(
-                <div key={"mosaicImage"+index} className={"mosaic-image-container fade-int " + props.id}>
+                <div key={"mosaicImage"+index} className={"mosaic-image-container"/* fade-int " + props.id*/}>
                     <img className="mosaic-image" src={require('./assets/buszke/thumbnail_' + i)} title={(props.titles===undefined)?("mosaicImage"+(index+1)):(props.titles[index])} alt={(props.titles===undefined)?("mosaicImage"+(index+1)):(props.titles[index])} onClick={()=>window.open(require('./assets/buszke/' + i),'_blank')} />
                 </div>
             ))}
